30efa8ceffb6f4af2fc61b3866958b2c7511e8f4
gardim1 <vini.gardim1@gmail.com>
2024-09-09 01:16:48 -0300

    Conectando com FrontEnd-React

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+const users = [];  
+
+
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 app.post('/usuarios', async (req, res)=>{
